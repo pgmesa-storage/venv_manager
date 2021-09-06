f92bf3be3e84a1c5afbed7a1214b9879c5cc931a
Pablo García <pablogmesa00@gmail.com>
2021-09-06 21:28:47 +0200

    Programa terminado version 1.0

--- venvm.bat
+++ venvm.bat
@@ -5,14 +5,14 @@
 call python --version > nul
 if '%errorlevel%' NEQ '0' (
     echo ERROR: Python is not installed, please install it before continuing
-    goto failure
+    exit /B 1
 )
 @REM Ver si pip esta instalado
 call pip --version > nul
 
 if '%errorlevel%' NEQ '0' (
     echo ERROR: Python package module 'pip' is not installed, please install it before continuing
-    goto failure
+    exit /B 1
 )
 
 @REM  Ver si virtualenv esta instalado y si no lo instalamos
